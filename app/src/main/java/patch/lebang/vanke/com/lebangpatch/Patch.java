6688d93ed2b98747675bf99a33e8198d8358df69
lgl <liugl01@vanke.com>
2015-08-27 15:48:23 +0800

    创建工程

---
+++ app/src/main/java/patch/lebang/vanke/com/lebangpatch/Patch.java
@@ -0,0 +1,13 @@
+package patch.lebang.vanke.com.lebangpatch;
+
+import com.taobao.patch.IPatch;
+import com.taobao.patch.PatchParam;
+
+public class Patch implements IPatch {
+
+	@Override
+	public void handlePatch(final PatchParam arg0) throws Throwable {
+
+	}
+
+}
